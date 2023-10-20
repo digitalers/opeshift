f97fcd48691fa59f155b25bf11115620727dc737
José Luis Núñez <jose.nunez@educacionit.com>
2023-10-20 07:02:24 -0300

    commit inicial

---
+++ src/main/java/es/indra/srpingopenshift/SrpingOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package es.indra.srpingopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingOpenshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/indra/srpingopenshift/SrpingOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package es.indra.srpingopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
